2c72e0ff162158fad872414e13006e16c0605506
nad-garraz <nad.neutron@pm.me>
2020-06-19 23:25:02 +1200

    Click en status

--- blocks.h
+++ blocks.h
@@ -4,15 +4,15 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 
-	{"", "~/.config/dwmbar/dwm_mpc",			1,		12},
-	{"", "~/.config/dwmbar/dwm_btc",			200,		13},
-	//{"", "~/.config/dwmbar/dwm_transmission",		10,		16},
-	{"", "~/.config/dwmbar/dwm_resources",			15,		2},
-	{"", "~/.config/dwmbar/dwm_battery",			30,		3},
-	{"", "~/.config/dwmbar/dwm_alsa",			0,		10},
-	{"", "~/.config/dwmbar/dwm_weather",			600,		1},
-	{"", "~/.config/dwmbar/dwm_keyboard",			2,		11},
-	{"", "~/.config/dwmbar/dwm_date",			60,		4},
+	{"", "~/.config/dwmbar/mpc",			1,		16},
+	{"", "~/.config/dwmbar/criptomoneda",			0,		13},
+	//{"", "~/.config/dwmbar/transmission",		10,		16},
+	{"", "~/.config/dwmbar/resources",			15,		2},
+	{"", "~/.config/dwmbar/battery",			30,		3},
+	{"", "~/.config/dwmbar/volumen",			0,		19},
+	{"", "~/.config/dwmbar/weather",			0,		21},
+	{"", "~/.config/dwmbar/keyboard",			2,		11},
+	{"", "~/.config/dwmbar/date",			60,		20},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
