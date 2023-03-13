560f90e0fa5ba6faa385233da98f323eee179342
Nad <nad.neutron@proton.me>
2023-03-13 13:45:02 +0100

    minor changes

--- blocks.h
+++ blocks.h
@@ -3,20 +3,19 @@
 
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval (s)*/	/*Update Signal*/
-
 	{"", "~/.config/dwmbar/mpc",		2,		1},
-	{"", "~/.config/dwmbar/criptomoneda",	1802,		2},
-	{"", "~/.config/dwmbar/transmission",	10,		16},
+	{"", "~/.config/dwmbar/criptomoneda",	1801,		5},
+	{"", "~/.config/dwmbar/transmission",	3,		24},
 	{"", "~/.config/dwmbar/ram_cpu",	30,		3},
 	{"", "~/.config/dwmbar/storage",	-1,		4},
 	{"", "~/.config/dwmbar/battery",	30,		5},
 	{"", "~/.config/dwmbar/volumen",	10,		6},
 	{"", "~/.config/dwmbar/monitor_mic",	10,		18},
-	{"", "~/.config/dwmbar/mic",		4,		17},
-	{"", "~/.config/dwmbar/weather",	1807,		7},
+	{"", "~/.config/dwmbar/mic",		10,		17},
+	{"", "~/.config/dwmbar/weather",	600,		7},
 	{"", "~/.config/dwmbar/keyboard",	5,		8},
 	{"", "~/.config/dwmbar/date",		180,		9},
-	{"", "~/.config/dwmbar/time",		40,		10},
+	{"", "~/.config/dwmbar/time",		40,		10}
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
